496e471a9484659a5f2ea5177e9447f556605ff1
rcalizayapa <rcalizayapa@unjbg.edu.pe>
2024-06-23 22:00:25 -0500

    Se subio el segundo ejercicio: Función.cpp

---
+++ Función.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+float suma(float);
+int factorial(int);
+
+//algoritmo proncipal
+int main(){
+    float n, sum;
+    char cn;
+    do{
+        system("cls");
+        cout<<"Ingrese un numero a operar: ";cin>>n;
+        sum =suma(n);
+        cout<<"La sumatoria es de: "<<sum<<endl;
+        //implementacion de mas operaciones
+        cout<<"Desea probar con mas numeros? s/n: ";cin>>cn;
+    }while(cn=='s');
+    return 0;
+}
+
+//funcion suma total
+float suma (float a){
+    float s=0;
+    for(int i=1; i<=a; i++){
+        s=s+(factorial(i)/(2.0*i));
+    }
+    return s;
+}
+
+//funcion factorial
+int factorial(int b){
+    int f=1;
+    for(int i=2; i<=b; i++){
+        f=f*i;
+    }
+    return f;
+}
